9190f13f9856c30232457b4a9115d8bc47edb062
Denis Makarenko <mail39.93@mail.ru>
2018-01-09 20:54:12 +0300

    pb-3 navigation design

---
+++ piggy-bank-uwp/ViewModel/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace piggy_bank_uwp.ViewModel
+{
+	internal abstract class BaseViewModel : INotifyPropertyChanged
+	{
+		internal virtual void RaisePropertyChanged(string propertyName)
+		{
+			if (String.IsNullOrEmpty(propertyName))
+				return;
+
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		internal virtual void RaisePropertyChanged(object obj)
+		{
+			if (obj == null)
+				return;
+
+			var properties = obj.GetType().GetRuntimeProperties();
+
+			foreach (var property in properties)
+			{
+				RaisePropertyChanged(property.Name);
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
