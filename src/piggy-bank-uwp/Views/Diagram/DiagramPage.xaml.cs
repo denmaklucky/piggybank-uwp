ff1f1f005ef6f5deeec860daf3c366f1723adc16
Denis Makarenko <mail39.93@mail.ru>
2018-05-22 22:00:33 +0300

    pb-23 add a filter in DiagramPage

--- src/piggy-bank-uwp/Views/Diagram/DiagramPage.xaml.cs
+++ src/piggy-bank-uwp/Views/Diagram/DiagramPage.xaml.cs
@@ -34,5 +34,36 @@ namespace piggy_bank_uwp.View.Diagram
             Diagram.Palette = palette;
             LabelsListView.ItemsSource = _diagram.Datas;
         }
+
+        private void OnFilterClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            FilterFlyout.ShowAt((AppBarButton)sender);
+        }
+
+        private void OnCheckMarkClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            FilterFlyout.Hide();
+
+            _diagram.ApplyFilter(StartDatePicker.Date.Date, EndDatePicker.Date.Date);
+
+            Diagram.Series[0].ItemsSource = null;
+            Diagram.Series[0].ItemsSource = _diagram.Datas;
+
+            ChartPalette palette = new ChartPalette { Name = "CustomsDark" };
+
+            foreach (var color in _diagram.Datas)
+            {
+                palette.FillEntries.Brushes.Add(new SolidColorBrush(ColorUtility.GetColorFromHexString(color.Color)));
+            }
+
+            Diagram.Palette = palette;
+            LabelsListView.ItemsSource = null;
+            LabelsListView.ItemsSource = _diagram.Datas;
+        }
+
+        private void OnCancelClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            FilterFlyout.Hide();
+        }
     }
 }
